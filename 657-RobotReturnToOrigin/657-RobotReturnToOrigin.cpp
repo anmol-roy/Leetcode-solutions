1580767c582e80188b4dd25b468e5f1101a50ea8
Anmol Roy <155471912+anmol-roy@users.noreply.github.com>
2026-05-04 15:42:56 +0530

    Create 657-RobotReturnToOrigin

---
+++ 657-RobotReturnToOrigin/657-RobotReturnToOrigin.cpp
@@ -0,0 +1,14 @@
+// Last updated: 04/05/2026, 15:42:54
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+       int x = 0, y = 0;
+       for (char &ch : moves){
+        if(ch == 'U') y++;
+        else if(ch == 'D') y--;
+        else if(ch == 'L') x--;
+        else if(ch == 'R') x++;
+       } 
+       return x == 0 && y == 0;
+    }
+};
