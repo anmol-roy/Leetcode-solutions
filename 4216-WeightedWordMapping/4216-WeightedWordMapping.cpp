3928cb872dc7bcd747db805d8cfd1bd769b1c57a
Anmol Roy <155471912+anmol-roy@users.noreply.github.com>
2026-07-06 09:52:03 +0530

    Create 4216-WeightedWordMapping

---
+++ 4216-WeightedWordMapping/4216-WeightedWordMapping.cpp
@@ -0,0 +1,23 @@
+// Last updated: 06/07/2026, 09:52:00
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+        string res(words.size(), 0);
+        int i = 0;
+
+        for (auto& word : words) {
+            int sum = 0;
+            for (auto& c : word)
+                sum += weights[(c & 31) - 1];
+            int q = (sum * 2521) >> 16;
+
+           
+            int r = sum - q * 26;
+
+            
+            res[i++] = 'z' - r;
+        }
+
+        return res;
+    }
+};
