303402c52d75171080a6c61f782bf7e49d6a4b61
Anmol Roy <155471912+anmol-roy@users.noreply.github.com>
2026-07-06 09:52:44 +0530

    Create 2917-CountPairsWhoseSumIsLessThanTarget

---
+++ 2917-CountPairsWhoseSumIsLessThanTarget/2917-CountPairsWhoseSumIsLessThanTarget.cpp
@@ -0,0 +1,22 @@
+// Last updated: 06/07/2026, 09:52:41
+class Solution {
+public:
+    int countPairs(vector<int>& nums, int target) {
+        int count = 0;
+
+        sort(nums.begin(), nums.end());
+        int left = 0; 
+        int right = nums.size()-1;
+
+        while(left < right){
+            if(nums[left] + nums[right] < target){
+                count += right - left;
+                left++;
+            }
+            else {
+                right--;
+            }
+        }
+        return count;
+    }
+};
