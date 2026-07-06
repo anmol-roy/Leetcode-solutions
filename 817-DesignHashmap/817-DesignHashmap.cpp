059b769622911fc8b5ae979b19ed62226fe5ad69
Anmol Roy <155471912+anmol-roy@users.noreply.github.com>
2026-07-06 09:53:51 +0530

    Create 817-DesignHashmap

---
+++ 817-DesignHashmap/817-DesignHashmap.cpp
@@ -0,0 +1,28 @@
+// Last updated: 06/07/2026, 09:53:47
+class MyHashMap {
+public:
+    int data[1000001];
+    MyHashMap() {
+        fill(data, data + 1000001, -1);
+    }
+    
+    void put(int key, int value) {
+        data[key] = value;
+    }
+    
+    int get(int key) {
+        return data[key];
+    }
+    
+    void remove(int key) {
+        data[key] = -1;
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
