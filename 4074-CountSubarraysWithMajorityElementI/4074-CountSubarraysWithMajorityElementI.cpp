2fb935e15a6748ab251a78468efe5adf59e74826
Anmol Roy <155471912+anmol-roy@users.noreply.github.com>
2026-07-06 09:52:09 +0530

    Create 4074-CountSubarraysWithMajorityElementI

---
+++ 4074-CountSubarraysWithMajorityElementI/4074-CountSubarraysWithMajorityElementI.cpp
@@ -0,0 +1,26 @@
+// Last updated: 06/07/2026, 09:52:06
+class Solution {
+public:
+    long long countMajoritySubarrays(vector<int>& nums, int target) {
+        int n = nums.size();
+        long long ans = 0;
+
+        for (int l = 0; l < n; l++) {
+            int targetCount = 0;
+
+            for (int r = l; r < n; r++) {
+                if (nums[r] == target) {
+                    targetCount++;
+                }
+
+                int len = r - l + 1;
+
+                if (targetCount > len / 2) {
+                    ans++;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
