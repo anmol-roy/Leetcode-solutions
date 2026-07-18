5427689c8c652d7117cc0cda76f14c531732ba3e
Anmol Roy <155471912+anmol-roy@users.noreply.github.com>
2026-07-19 00:23:05 +0530

    Create 3583-SortedGcdPairQueries

---
+++ 3583-SortedGcdPairQueries/3583-SortedGcdPairQueries.cpp
@@ -0,0 +1,40 @@
+// Last updated: 19/07/2026, 00:23:03
+class Solution {
+public:
+    vector<int> gcdValues(vector<int>& nums, vector<long long>& queries) {
+        long long int mx = *max_element(nums.begin(), nums.end());
+
+        vector<int> mp(mx + 1, 0);
+        for (auto it : nums) {
+            mp[it]++;
+        }
+
+        vector<long long> cnt(mx + 1, 0);
+
+        for (int i = mx; i >= 1; i--) {
+            long long curr = 0;
+
+            for (int k = i; k <= mx; k += i) {
+                curr += mp[k]; // inclusion of all possible pairs
+            }
+
+            cnt[i] = curr * (curr - 1) / 2;
+
+            for (int k = 2 * i; k <= mx; k += i) {
+                cnt[i] -= cnt[k]; // exclusion of all pairs covered by greater gcd
+            }
+        }
+
+        for (int i = 1; i <= mx; i++) {
+            cnt[i] += cnt[i - 1];
+        }
+
+        vector<int> sol;
+
+        for (auto it : queries) {
+            sol.push_back(lower_bound(cnt.begin(), cnt.end(), it + 1) - cnt.begin());
+        }
+
+        return sol;
+    }
+};
