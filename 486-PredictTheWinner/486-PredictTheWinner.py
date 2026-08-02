74743da0de126a121eb8f75ec589edb128259ab2
Anmol <155471912+anmol-roy@users.noreply.github.com>
2026-08-02 09:00:43 +0530

    Create 486-PredictTheWinner

---
+++ 486-PredictTheWinner/486-PredictTheWinner.py
@@ -0,0 +1,10 @@
+# Last updated: 02/08/2026, 09:00:42
+class Solution:
+    def predictTheWinner(self, A: List[int]) -> bool:
+        @cache
+        def maxDiff(i: int, j: int) -> int:
+            if i == j: return A[i]
+            return max(A[i] - maxDiff(i + 1, j),
+                       A[j] - maxDiff(i, j - 1))
+
+        return maxDiff(0, len(A) - 1) >= 0
