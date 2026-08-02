c6e24964da5a538942d2f0893b5088f0a54ad89f
Anmol <155471912+anmol-roy@users.noreply.github.com>
2026-08-02 09:03:09 +0530

    Create 70-ClimbingStairs

---
+++ 70-ClimbingStairs/70-ClimbingStairs.cpp
@@ -0,0 +1,21 @@
+// Last updated: 02/08/2026, 09:03:08
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n <= 2) 
+            return n;
+        
+        int prev2 = 1;
+        int prev1 = 2;
+
+        for(int i = 3; i <= n; i++) {
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+        
+
+        
+    }
+};
