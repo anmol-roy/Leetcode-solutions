e48b80c4190fe929fdc08e233c1783c34ca5c5a4
Anmol <155471912+anmol-roy@users.noreply.github.com>
2026-08-24 19:59:18 +0530

    Create 2156-StoneGameIx

---
+++ 2156-StoneGameIx/2156-StoneGameIx.cpp
@@ -0,0 +1,27 @@
+// Last updated: 24/08/2026, 19:59:18
+class Solution {
+public:
+    bool stoneGameIX(vector<int>& stones) {
+
+    int count[3] = {0, 0, 0};
+
+    for (int i = 0;
+         i < stones.size();
+         i++) {
+
+        stones[i] %= 3;
+
+        count[stones[i]]++;
+    }
+
+    if (count[0] % 2 == 0) {
+
+    
+        return count[1] != 0 &&
+               count[2] != 0;
+    }
+
+   
+    return abs(count[2] - count[1]) >= 3;
+}
+};
