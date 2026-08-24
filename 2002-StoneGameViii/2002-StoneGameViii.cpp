0cdd193d40151614fca581df50b3d9087d844934
Anmol <155471912+anmol-roy@users.noreply.github.com>
2026-08-24 19:59:31 +0530

    Create 2002-StoneGameViii

---
+++ 2002-StoneGameViii/2002-StoneGameViii.cpp
@@ -0,0 +1,15 @@
+// Last updated: 24/08/2026, 19:59:31
+class Solution {
+public:
+    int stoneGameVIII(vector<int>& A) {
+        int n = A.size();
+        for (int i = 1; i < n; i++)
+            A[i] += A[i - 1];
+
+        int ans = A.back();
+        for (int i = n - 2; i > 0; i--)
+            ans = max(ans, A[i] - ans);
+
+        return ans;
+    }
+};
